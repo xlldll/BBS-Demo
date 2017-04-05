36e6d4af80122b4c5bab5f83c8a86f0e121befa9
Ajon <wychuqiang@163.com>
2017-04-05 09:05:44 +0800

    BBS

---
+++ php/config.php
@@ -0,0 +1,16 @@
+﻿<?php
+    // 指定允许其他域名访问
+    header('Access-Control-Allow-Origin:*');
+    // 响应类型
+    header('Access-Control-Allow-Methods:POST');
+    // 响应头设置
+    header('Access-Control-Allow-Headers:x-requested-with,content-type');
+    header('Content-Type:text/html; charset=utf-8;');
+	define('DB_HOST','localhost');
+	define('DB_USER','root');
+	define('DB_PWD','');
+	define('DB_NAME',"linshop");
+	$conn = @mysql_connect(DB_HOST,DB_USER,DB_PWD) or die('数据库链接失败：' . mysql_error());
+	@mysql_select_db(DB_NAME) or die('数据库错误：' . mysql_error());
+	@mysql_query('SET NAMES UTF8') or die('字符集错误：' . mysql_error());
+?>
